f624f90c9b575dea3ad03429b5fc112287b9d787
prashanta2k <46659425+prashanta2k@users.noreply.github.com>
2019-01-14 05:20:47 +0530

    printNmeAge.py

---
+++ printNameAge.py
@@ -0,0 +1,9 @@
+#what is  your name
+name=input("what is your name")
+#what is your age 
+age1=input("what is your age")
+#age1 is string. We need to convert into integer using int()
+age=int(age1)
+#name will turn 100 years after 100-age
+x=100-age
+print(name," will turn 100 years after ",x,"years")
