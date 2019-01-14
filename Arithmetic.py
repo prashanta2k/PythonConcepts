0afc731a6f7e1ed9f7034075826f6779839284c5
prashanta2k <46659425+prashanta2k@users.noreply.github.com>
2019-01-14 05:56:11 +0530

    Create Arithmetic.py

---
+++ Arithmetic.py
@@ -0,0 +1,14 @@
+number1=input("what is the first number?")
+number1=int(number1)
+number2=input("what is the second number?")
+number2=int(number2)
+#addition
+print(number1, "+", number2, "=", number1+number2)
+#substrction
+print(number1, "-", number2, "=", number1-number2)
+#divison
+print(number1, "/", number2, "=", number1/number2)
+#multiplication
+print(number1, "*", number2, "=", number1*number2)
+#floor division
+print(number1, "//", number2, "=", number1//number2)
